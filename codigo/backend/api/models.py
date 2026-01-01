5bb1ec150c7efc061de4f969e44e22a78571e280
jromeroa17 <jromeroa17@educarex.es>
2026-01-01 11:09:20 +0100

    creado backend en primera versión

---
+++ codigo/backend/api/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Character(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
+
+    def __str__(self):
+        return self.name
+
---
+++ codigo/backend/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("character/", views.CharacterList.as_view(), name="character-list"),
+    path("character/delete/<int:pk>/", views.CharacterDelete.as_view(), name="delete-character"),
+    path("character/create/", views.CharacterCreate.as_view(), name="create-character"),
+    path("character/update/<int:pk>/", views.CharacterUpdate.as_view(), name="update-character"),
+
+]
